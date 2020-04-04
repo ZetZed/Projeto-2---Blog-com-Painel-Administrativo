bc1517aa7a8497123a535f60a3befa35b7124b96
ZetZed <rodhaste@hotmail.com>
2020-04-04 16:06:37 -0300

    Começando
    Primeiros passos

---
+++ Index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express(); //Cria instância do express
+const BodyParser = require("body-parser");
+const connection = require("./database/database");
+
+//VIEW ENGINE
+app.set('view engine', 'ejs'); //Seleciona a View Engine 'Ejs' , que serve para ler HTML(na pasta view)
+
+//STATIC
+app.use(express.static('public')); //Configuração pro Express aceitar trabalhar com arquivos estáticos (Css, imagem, JS no Front-End), que estarão na pasta 'public'. 
+
+//BODY PARSER
+app.use(BodyParser.urlencoded({ extended: false })); //configura BodyParser = Para poder trabalhar com formulários
+app.use(BodyParser.json()); //Diz pro Body Parser que além de aceitar dados de formulários, também aceitar dados de formato Json.
+
+//DATABASE
+connection
+    .authenticate() //Autentica com BD
+    .then(() => { //Se autenticação der certo, envia mensagem abaixo.
+        console.log("Conexão com BD feita com sucesso!");
+    }).catch((error) => { //Se autenticação der erro, manda mensagem de erro.
+        console.log(error);
+    })
+
+//ROTAS
+app.get("/", (req, res) => {
+    //res.send("Bem vindo ao meu site"); //Para mandar msg para tela principal, sem usar o HTML...
+    res.render("index"); //Para mandar msg que está em HTML no arquivo 'Index.ejs' na pasta views, através do view engine ejs para a tela principal..
+})
+
+//RODAR APLICAÇÃO
+app.listen(8080, () => {
+    console.log("O servidor está rodando!")
+})
